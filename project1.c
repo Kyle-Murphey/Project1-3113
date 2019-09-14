15ce23f3b34a8608dfd0c8ed18a09e8aa5c06e18
Kyle-Murphey <kyle_murphey@live.com>
2019-09-14 16:10:40 -0500

    linked vscode to repo

--- project1.c
+++ project1.c
@@ -1,1 +1,14 @@
-//empty project1
+#include <stdio.h>
+#include <string.h>
+
+int main(int argc, char **argv)
+{
+    char buffer[128] = {1};
+
+    char input[1];
+
+    while (fgets(input, 2, stdin))
+    {
+        printf("got: %02x\n", input[0]);
+    }
+}
