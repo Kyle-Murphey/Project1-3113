37ed3b9a2176e71a077d468cc94057d554810fa8
Kyle-Murphey <kyle_murphey@live.com>
2019-09-14 16:27:57 -0500

    added list and zero functionality

--- project1.c
+++ project1.c
@@ -3,12 +3,28 @@
 
 int main(int argc, char **argv)
 {
-    char buffer[128] = {1};
+    char buffer[128] = {0};
 
     char input[1];
 
     while (fgets(input, 2, stdin))
     {
-        printf("got: %02x\n", input[0]);
+        if (input[0] == 'z')
+        {
+            memset(buffer, 0, sizeof(buffer));
+        }
+        else if (input[0] == 'l')
+        {
+            for (int i = 0; i < 128; ++i)
+            {
+                printf("%02x ", buffer[i]);
+
+                if ((i + 1) % 16 == 0)
+                {
+                    printf("\n");
+                }
+            }
+        }
+        //printf("got: %02x\n", input[0]);
     }
 }
