559fd8358fe686fcdc179a55d71d78d938292ff9
Kyle-Murphey <kyle_murphey@live.com>
2019-09-16 18:18:24 -0500

    added I input/can display int values from buffer

--- project1.c
+++ project1.c
@@ -1,5 +1,7 @@
 #include <stdio.h>
 #include <string.h>
+#include <stdlib.h>
+#include <math.h>
 
 /* separators for tokens/inputs */
 const char SEPARATORS[] = " \t\n";
@@ -13,7 +15,7 @@ int main(int argc, char **argv)
     //const unsigned int BUFFER_SIZE = 128; //size of buffer
     ///*unsigned */int MAX_ARGS = 4; //max number of arguments
 
-    byte buffer[128] /*= {1}*/;
+    byte buffer[128] = {0};
     byte input[50];
     byte * args[4];
     byte ** arg;
@@ -30,13 +32,45 @@ int main(int argc, char **argv)
 
         if (*args[0] == 'i')
         {
-            
-        }
+            int intVal = atoi(args[2]);
+            int intLoc = atoi(args[1]);
+            int * intLocation = (int*)(&buffer[intLoc]);
+            printf("0x%02x\n", atoi(args[2]));
 
+            *intLocation = intVal;
+        }
+        //prints integer output
+        else if (*args[0] == 'I')
+        {
+            byte output[50] = {0}; //output array
+            int j = 0; //index for output array
+            int total = 0; //total integer value
+            const int base = 16; //hex is base 16
+            int factor = 0; //factor increases by 2 for each group of bytes
+
+            //starts at index specified by arg, and increments until \0 (empty mem in buffer) and stores into output array
+            for (int i = atoi(args[1]); buffer[i] != '\0'; ++i)
+            {
+                output[j++] = buffer[i];
+            }
+
+            int length = strlen(output); //number of hexes to parse
+            
+            //takes each hex value as a decimal and sums up the entries to give total int value
+            for (int i = 0; i < length; ++i)
+            {
+                total += (output[i] * pow(base, factor));
+                factor += 2;
+            }
+            //output integer
+            printf ("%d\n", total);
+        }
+        //zero out the buffer
         if (*args[0] == 'z')
         {
             memset(buffer, 0, sizeof(buffer));
         }
+        //list contents of buffer
         else if (*args[0] == 'l')
         {
             for (int i = 0; i < 128; ++i)
