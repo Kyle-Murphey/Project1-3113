f7a8400bc52b4fbcfc9181563cf835ee2ae78588
Kyle-Murphey <kyle_murphey@live.com>
2019-09-17 13:51:47 -0500

    added byte opps and shortened implementation

--- project1.c
+++ project1.c
@@ -9,7 +9,9 @@ const char SEPARATORS[] = " \t\n";
 /* renaming unsigned char to byte */
 typedef unsigned char byte;
 
-
+/*
+ * main function
+*/
 int main(int argc, char **argv)
 {
     byte buffer[128] = {0};
@@ -27,21 +29,23 @@ int main(int argc, char **argv)
         //input integer value
         if (*args[0] == 'i')
         {
-            int value = atoi(args[2]);
-            int location = atoi(args[1]);
-            int * ptr_location = (int*)(&buffer[location]);
-            //printf("0x%02x\n", atoi(args[2]));
-            *ptr_location = value;
+            int value = atoi(args[2]); //value to store
+            int location = atoi(args[1]); //location in buffer to get stored
+            int * ptr_location = (int*)(&buffer[location]); //pointer to the location in the buffer to store value
+            *ptr_location = value; //set val of location to inputted value
         }
         //prints integer output
         else if (*args[0] == 'I')
         {
-            byte output[50] = {0}; //output array
+            int * ptr_loc = (int*)(&buffer[atoi(args[1])]); //pointer to location in buffer to read
+            printf("%d\n", *ptr_loc); //print integer at given location
+
+/*            byte output[50] = {0}; //output array
             int j = 0; //index for output array
             int total = 0; //total integer value
             const int base = 16; //hex is base 16
             int factor = 0; //factor increases by 2 for each group of bytes
-            
+
             //starts at index specified by arg, and increments until \0 (empty mem in buffer) and stores into output array
             for (int i = atoi(args[1]); buffer[i] != '\0'; ++i)
             {
@@ -58,9 +62,25 @@ int main(int argc, char **argv)
             }
             //output the integer
             printf ("%d\n", total);
+*/
+        }
+        //input byte value
+        else if (*args[0] == 'b')
+        {
+            byte value = atoi(args[2]);
+            int location = atoi(args[1]);
+            byte * ptr_location = (byte*)(&buffer[location]);
+            *ptr_location = value;
+            //printf("%d\n", value);
+        }
+        //prints byte value
+        else if (*args[0] == 'B')
+        {
+            byte * ptr_loc = (byte*)(&buffer[atoi(args[1])]);
+            printf("%d\n", *ptr_loc);
         }
         //zero out the buffer
-        if (*args[0] == 'z')
+        else if (*args[0] == 'z')
         {
             memset(buffer, 0, sizeof(buffer));
         }
