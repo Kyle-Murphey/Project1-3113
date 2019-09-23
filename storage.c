43d6f37a6e7686afea3d8ec552e4ded09cffd985
Kyle <kyle.r.murphey@ou.edu>
2019-09-23 13:11:23 -0500

    added implementation for storage API (probably very wrong)

--- storage.c
+++ storage.c
@@ -12,20 +12,62 @@ typedef struct
 
 STORAGE * init_storage(char * name)
 {
+    STORAGE * file;
+    file->fd = open(name, O_CREAT | O_RDWR);
 
+    if (file->fd == -1)
+    {
+      fprintf(stderr, "Could not open or create file\n");
+      return NULL;
+    }
+    
+    return file;
+    
 }
 
+/* REMEMBER TO ERROR CHECK IN MAIN */
 int close_storage(STORAGE *storage)
 {
-
+  int isClosed = close(storage->fd);
+  return isClosed;
 }
 
 int get_bytes(STORAGE *storage, unsigned char *buf, int location, int len)
 {
+  int setStart = lseek(storage->fd, location, SEEK_SET);
 
+  if (setStart < 0)
+  {
+    fprintf(stderr, "error setting offset\n");
+    return setStart;
+  }
+
+  int readBytes = read(storage->fd, buf, len);
+
+  if (readBytes < 0) 
+  {
+    fprintf(stderr, "error reading file\n");
+  }
+
+  return readBytes;
 }
 
 int put_bytes(STORAGE *storage, unsigned char *buf, int location, int len)
 {
+  int setStart = lseek(storage->fd, location, SEEK_SET);
 
+  if (setStart < 0)
+  {
+    fprintf(stderr, "error setting offset\n");
+    return setStart;
+  }
+
+  int writeBytes = write(storage->fd, buf, len);
+
+  if (writeBytes < 0)
+  {
+    fprintf(stderr, "error writing to file\n");
+  }
+
+  return writeBytes;
 }
